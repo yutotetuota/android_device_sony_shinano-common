e3bbe580ebaad77a6d3884c86af5f8fc9cf68a81
Keita <keita_nya@live.jp>
2017-10-23 16:20:37 -0300

    shinano-common: Clean and update VibratorHW implementation
    * Use class path instead of virtual one
    * Set the following values defined by the qpnp driver: 31max;29def;12min

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 The CyanogenMod Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,34 +16,46 @@
 
 package org.cyanogenmod.hardware;
 
-import java.io.File;
-
 import org.cyanogenmod.internal.util.FileUtils;
+
+import android.util.Log;
 
 public class VibratorHW {
 
-    private static String LEVEL_PATH = "/sys/devices/virtual/timed_output/vibrator/vtg_level";
+    private static final String TAG = "VibratorHW";
+
+    private static final String LEVEL_PATH = "/sys/class/timed_output/vibrator/vtg_level";
 
     public static boolean isSupported() {
-        return true;
+        return FileUtils.isFileWritable(LEVEL_PATH);
     }
 
-    public static int getMaxIntensity()  {
+    public static int getMaxIntensity() {
         return 31;
     }
-    public static int getMinIntensity()  {
-        return 21;
+
+    public static int getMinIntensity() {
+        return 12;
     }
-    public static int getWarningThreshold()  {
+
+    public static int getWarningThreshold() {
         return 31;
     }
-    public static int getCurIntensity()  {
-        return Integer.parseInt(FileUtils.readOneLine(LEVEL_PATH));
+
+    public static int getCurIntensity() {
+        try {
+            return Integer.parseInt(FileUtils.readOneLine(LEVEL_PATH));
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+        return -1;
     }
-    public static int getDefaultIntensity()  {
-        return 31;
+
+    public static int getDefaultIntensity() {
+        return 29;
     }
-    public static boolean setIntensity(int intensity)  {
+
+    public static boolean setIntensity(int intensity) {
         return FileUtils.writeLine(LEVEL_PATH, String.valueOf(intensity));
     }
 }
